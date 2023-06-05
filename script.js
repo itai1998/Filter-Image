386b8ee5dcd423c843f73c52554b538bce2d10e2
I-Tai Lin <smes93434@gmail.com>
2023-06-05 10:36:53 -1000

    Add filter search bar

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const filterButton = document.querySelectorAll(".filter_buttons button");
 // Get all the .card class under .filterable_cards class
 const filterableCards = document.querySelectorAll(".filterable_cards .card");
 
+const searchButton = document.querySelector(".search_button");
+const searchInput = document.querySelector(".search_bar");
+
 // Define the filterCards function
 const filterCards = e =>{
     document.querySelector(".active").classList.remove("active");
@@ -22,5 +25,22 @@ const filterCards = e =>{
     })
 }
 
+const searchFilter = e =>{
+    const searchResult = searchInput.value;
+
+    // Iterate over each filterable card
+    filterableCards.forEach(card => {
+        // Add "hide" class to hide the card iniially
+        card.classList.add("hide");
+      
+        // Check if the card matches the selected filter or "all" is selected
+        if(searchResult === card.dataset.name){
+            card.classList.remove ("hide");
+        }
+    })
+    searchInput.value = "";
+}
+
 // Add click event listener to each filter button
 filterButton.forEach(button => button.addEventListener("click", filterCards));
+searchButton.addEventListener("click", searchFilter);
